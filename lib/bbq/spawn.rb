0671617a32d4e8319e16ca2f20bfd47dfccaaadd
Paweł Pacana <pawel.pacana@gmail.com>
2013-04-14 15:37:51 +0200

    Coordinated execution for the glory of sleepless tests.

--- lib/bbq/spawn.rb
+++ lib/bbq/spawn.rb
@@ -7,12 +7,40 @@ module Bbq
     class Executor
       extend Forwardable
 
-      def_delegators :@process, :start, :stop
+      def_delegators :@process, :start, :stop, :io
 
       def initialize(*args)
         @process = ChildProcess.build(*args)
         yield @process if block_given?
         @process
+      end
+    end
+
+    class CoordinatedExecutor
+      extend Forwardable
+
+      def_delegators :@executor, :stop
+
+      def initialize(executor, banner)
+        @executor = executor
+        @banner   = banner
+        @reader, @writer = IO.pipe
+      end
+
+      def start
+        @executor.io.stdout = @executor.io.stderr = @writer
+        @executor.start
+        @writer.close
+      end
+
+      def join
+        return unless @banner
+        loop do
+          case @reader.readpartial(8192)
+          when @banner then break
+          end
+        end
+      rescue EOFError
       end
     end
 
@@ -22,15 +50,16 @@ module Bbq
       end
 
       def coordinate(executor, wait_banner = nil)
-        @executors << executor
+        @executors << CoordinatedExecutor.new(executor, wait_banner)
       end
 
       def start
-        @executors.each { |exec| exec.start }
+        @executors.each(&:start)
+        @executors.each(&:join)
       end
 
       def stop
-        @executors.each { |exec| exec.stop }
+        @executors.each(&:stop)
       end
     end
   end
