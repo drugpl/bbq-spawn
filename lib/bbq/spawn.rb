622f022ea93dbace651bf0920624f0bf5496727d
Paweł Pacana <pawel.pacana@gmail.com>
2013-04-14 14:39:25 +0200

    Naive, output swallowing orchestrator.

--- lib/bbq/spawn.rb
+++ lib/bbq/spawn.rb
@@ -1,7 +1,37 @@
 require "bbq/spawn/version"
+require "childprocess"
+require "forwardable"
 
 module Bbq
   module Spawn
-    # Your code goes here...
+    class Executor
+      extend Forwardable
+
+      def_delegators :@process, :start, :stop
+
+      def initialize(*args)
+        @process = ChildProcess.build(*args)
+        yield @process if block_given?
+        @process
+      end
+    end
+
+    class Orchestrator
+      def initialize
+        @executors = []
+      end
+
+      def coordinate(executor, wait_banner = nil)
+        @executors << executor
+      end
+
+      def start
+        @executors.each { |exec| exec.start }
+      end
+
+      def stop
+        @executors.each { |exec| exec.stop }
+      end
+    end
   end
 end
